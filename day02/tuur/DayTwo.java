1ae1bc02c657613544a580a430918815ac982601
Tuur Bressinck <bressinck.tuur@gmail.com>
2023-12-02 09:48:42 +0100

    Myeah, that looks nicer

--- day02/tuur/DayTwo.java
+++ day02/tuur/DayTwo.java
@@ -18,14 +18,11 @@ public class DayTwo {
 
 		System.out.println("Part 2: " + lines.stream()
 			.map(DayTwo::determineMinimums)
-			.mapToLong(min -> min.red * min.blue * min.green)
+			.mapToLong(Cubes::power)
 			.sum());
 	}
 	
 	private static final Pattern gameIdPattern = Pattern.compile("Game (?<id>\\d+):");
-	private static final Pattern redPattern = Pattern.compile("(?<red>\\d+)\\sred");
-	private static final Pattern bluePattern = Pattern.compile("(?<blue>\\d+)\\sblue");
-	private static final Pattern greenPattern = Pattern.compile("(?<green>\\d+)\\sgreen");
 	
 	private static long parseGameId(String line) {
 		Matcher matcher = gameIdPattern.matcher(line);
@@ -37,65 +34,46 @@ public class DayTwo {
 	
 	private static boolean isValidGame(String line, int maxRed, int maxGreen, int maxBlue) {
 		for(String draw : line.split(";"))  {
-			Matcher redMatcher = redPattern.matcher(draw);
-			if (redMatcher.find()) {
-				int red = Integer.parseInt(redMatcher.group("red"));
-				if (red > maxRed) {
-					return false;
-				}
-			}
-			Matcher blueMatcher = bluePattern.matcher(draw);
-			if (blueMatcher.find()) {
-				int blue = Integer.parseInt(blueMatcher.group("blue"));
-				if (blue > maxBlue) {
-					return false;
-				}
-			}
-			Matcher greenMatcher = greenPattern.matcher(draw);
-			if (greenMatcher.find()) {
-				int green = Integer.parseInt(greenMatcher.group("green"));
-				if (green > maxGreen) {
-					return false;
-				}
-			}
+			if (howManyBallsOfColor(draw, "red") > maxRed) return false;
+			if (howManyBallsOfColor(draw, "green") > maxGreen) return false;
+			if (howManyBallsOfColor(draw, "blue") > maxBlue) return false;
 		}
-		
 		return true;
 	}
 	
-	private static Minimums determineMinimums(String line) {
+	private static Cubes determineMinimums(String line) {
 		int minGreen = 0;
 		int minBlue = 0;
 		int minRed = 0;
 		
 		for(String draw : line.split(";"))  {
-			Matcher redMatcher = redPattern.matcher(draw);
-			if (redMatcher.find()) {
-				int red = Integer.parseInt(redMatcher.group("red"));
-				minRed = Math.max(minRed, red);
-			}
-			Matcher blueMatcher = bluePattern.matcher(draw);
-			if (blueMatcher.find()) {
-				int blue = Integer.parseInt(blueMatcher.group("blue"));
-				minBlue = Math.max(minBlue, blue);
-			}
-			Matcher greenMatcher = greenPattern.matcher(draw);
-			if (greenMatcher.find()) {
-				int green = Integer.parseInt(greenMatcher.group("green"));
-				minGreen = Math.max(minGreen, green);
-			}
+			minRed = Math.max(minRed, howManyBallsOfColor(draw, "red"));
+			minGreen = Math.max(minGreen, howManyBallsOfColor(draw, "green"));
+			minBlue = Math.max(minBlue, howManyBallsOfColor(draw, "blue"));
 		}
 		
-		return new Minimums(minRed, minGreen, minBlue);
+		return new Cubes(minRed, minGreen, minBlue);
 	}
 	
-	private static class Minimums {
+	private static int howManyBallsOfColor(String draw, String color) {
+		Matcher colorMatcher = Pattern.compile("(?<amount>\\d+)\\s" + color).matcher(draw);
+		if (colorMatcher.find()) {
+			return Integer.parseInt(colorMatcher.group("amount"));
+		}
+		return 0;
+	}
+	
+	private static class Cubes {
 		private final int red, green, blue;
 		
-		public Minimums(int red, int green, int blue) {
+		public Cubes(int red, int green, int blue) {
 			this.red = red;
 			this.green = green;
 			this.blue = blue;
+		}
+		
+		public int power() {
+			return red * green * blue;
 		}
 	}
 }
