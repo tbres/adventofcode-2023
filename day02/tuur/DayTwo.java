7971a1b0480e08bfe1e49f1c61d2181203019786
Tuur Bressinck <bressinck.tuur@gmail.com>
2023-12-02 07:56:34 +0100

    Skeleton day 02

---
+++ day02/tuur/DayTwo.java
@@ -0,0 +1,17 @@
+package tuur;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class DayTwo {
+	
+	public static void main(String[] args) throws Exception {
+		List<String> lines = Files.readAllLines(Paths.get(DayTwo.class.getResource("input-day02.txt").toURI()));
+
+		System.out.println("Part 1: ");
+
+		System.out.println("Part 2: ");
+
+	}
+}
