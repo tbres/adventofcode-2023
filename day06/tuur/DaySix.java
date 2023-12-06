392f9a4ba928603bba97786503598f41669062f9
Tuur Bressinck <bressinck.tuur@gmail.com>
2023-12-06 20:52:48 +0100

    Easy peasy

--- day06/tuur/DaySix.java
+++ day06/tuur/DaySix.java
@@ -1,17 +1,40 @@
 package tuur;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 
 public class DaySix {
 	
 	public static void main(String[] args) throws Exception {
-		List<String> lines = Files.readAllLines(Paths.get(DaySix.class.getResource("input-day06.txt").toURI()));
 
-		System.out.println("Part 1: ");
+		long[][] input = new long[4][];
+		input[0] = new long[] {54, 302};
+		input[1] = new long[] {94, 1476};
+		input[2] = new long[] {65, 1029};
+		input[3] = new long[] {92, 1404};
 
-		System.out.println("Part 2: ");
+		long marginOfError = 1l;
+		for(long[] game : input) {
+			long numberOfWaysToWin = numberOfWaysToWin(game[0], game[1]);
+			marginOfError *= numberOfWaysToWin;
+		}
+		System.out.println("Part 1: " + marginOfError);
+		
+		long totalTime = Long.parseLong(Arrays.stream(input)
+				.map(x -> Long.toString(x[0]))
+				.collect(Collectors.joining()));
+		long totalRecord = Long.parseLong(Arrays.stream(input)
+				.map(x -> Long.toString(x[1]))
+				.collect(Collectors.joining()));
+		System.out.println("Part 2: " + numberOfWaysToWin(totalTime, totalRecord));
 
+	}
+	
+	private static long numberOfWaysToWin(long timeLimit, long record) {
+		return LongStream.range(0, timeLimit)
+			.map(i -> i * (timeLimit - i))
+			.filter(j -> j > record)
+			.count();
 	}
 }
