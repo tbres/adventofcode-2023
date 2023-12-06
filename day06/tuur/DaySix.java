00da9afc65690683fabb713427a3e8f8fc1b64b3
Tuur Bressinck <bressinck.tuur@gmail.com>
2023-12-06 19:48:01 +0100

    Late start

---
+++ day06/tuur/DaySix.java
@@ -0,0 +1,17 @@
+package tuur;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class DaySix {
+	
+	public static void main(String[] args) throws Exception {
+		List<String> lines = Files.readAllLines(Paths.get(DaySix.class.getResource("input-day06.txt").toURI()));
+
+		System.out.println("Part 1: ");
+
+		System.out.println("Part 2: ");
+
+	}
+}
