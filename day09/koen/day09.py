f8ee223ca13a7044b783353b95f73c3298cc6f7e
Nabochi <koen.dhaen@gmail.com>
2023-12-09 11:17:54 +0100

    part one kenny angry

---
+++ day09/koen/day09.py
@@ -0,0 +1,20 @@
+
+def parse_line(line):
+    return [int(x) for x in line.split(' ')]
+
+
+def parse_input(path):
+    with open(path) as f:
+        for line in f.readlines():
+            yield parse_line(line)
+
+
+def extrapolate(history, logger):
+    result = []
+    logger.debug(f"Checking {history}")
+    while not all([x == 0 for x in history]):
+        result.append(history[-1])
+        history = [history[i + 1] - history[i] for i in range(len(history) - 1)]
+        logger.debug(f"    {history}")
+    logger.debug(f"  {result}")
+    return sum(result)
